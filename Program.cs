840c4b075dfc9bad295e02653a3f10b35cbe19f8
Oleksandr Shuryha <alshuriga@gmail.com>
2022-12-26 21:16:16 +0100

    test

--- Program.cs
+++ Program.cs
@@ -5,37 +5,37 @@ using Telegram.Bot;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// var botConfig = builder.Configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
+var botConfig = builder.Configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
 
-// builder.Services.AddHostedService<ConfigureWebhook>();
+builder.Services.AddHostedService<ConfigureWebhook>();
 
-// builder.Services.AddHttpClient("tgwebhook").AddTypedClient<ITelegramBotClient>(httpClient => new TelegramBotClient(botConfig.BotToken, httpClient));
+builder.Services.AddHttpClient("tgwebhook").AddTypedClient<ITelegramBotClient>(httpClient => new TelegramBotClient(botConfig.BotToken, httpClient));
 
-// builder.Services.AddHttpClient("tenorclient", tenorclient => {
-//     var tenorConfiguration = builder.Configuration.GetSection("TenorConfiguration").Get<TenorConfiguration>();
-//     tenorclient.BaseAddress = new Uri($"{tenorConfiguration.HostAddress}?key={tenorConfiguration.ApiToken}");
-// });
+builder.Services.AddHttpClient("tenorclient", tenorclient => {
+    var tenorConfiguration = builder.Configuration.GetSection("TenorConfiguration").Get<TenorConfiguration>();
+    tenorclient.BaseAddress = new Uri($"{tenorConfiguration.HostAddress}?key={tenorConfiguration.ApiToken}");
+});
 
-// builder.Services.AddScoped<TenorService>();
+builder.Services.AddScoped<TenorService>();
 
-// builder.Services.AddScoped<HandleUpdateService>();
+builder.Services.AddScoped<HandleUpdateService>();
 
-// builder.Services.AddScoped<AnimationEditService>();
+builder.Services.AddScoped<AnimationEditService>();
 
-// builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 var app = builder.Build();
 
-// app.UseRouting();
+app.UseRouting();
 
-// app.UseEndpoints(endpoints => {
-//     var token = botConfig.BotToken;
-//     endpoints.MapControllerRoute(name: "tgwebhook",
-//     pattern: $"bot/{token}",
-//     new { controller = "WebHook", action = "Post" }
-//     );
-//     endpoints.MapControllers();
-// });
+app.UseEndpoints(endpoints => {
+    var token = botConfig.BotToken;
+    endpoints.MapControllerRoute(name: "tgwebhook",
+    pattern: $"bot/{token}",
+    new { controller = "WebHook", action = "Post" }
+    );
+    endpoints.MapControllers();
+});
 
 
 
