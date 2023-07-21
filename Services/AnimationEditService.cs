678903e973bc05552fd7afcdb202639285cb0b18
Oleksandr Shuryha <alshuriga@gmail.com>
2023-07-21 17:45:21 +0200

    modified:   Services/AnimationEditService.cs

--- Services/AnimationEditService.cs
+++ Services/AnimationEditService.cs
@@ -97,7 +97,7 @@ public class AnimationEditService
         }
         catch (Exception ex)
         {
-            _logger.LogError($"CONVERSION ERROR:\n{ex.Source}\n{ex.Source}\n{ex.StackTrace}\n{ex.InnerException}");
+            _logger.LogError($"CONVERSION ERROR:\n{ex.Source}\n{ex.Message}\n{ex.StackTrace}\nMessage: {ex.InnerException?.Message}");
             return null;
         }
     }
