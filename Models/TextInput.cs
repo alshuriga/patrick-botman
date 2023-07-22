56cddc137774de9c8c21e9887bd9f1ba8c3d017a
Oleksandr Shuryha <alshuriga@gmail.com>
2023-07-22 22:59:10 +0200

    modified:   Models/TextInput.cs

--- Models/TextInput.cs
+++ Models/TextInput.cs
@@ -2,41 +2,51 @@ using System.Text.RegularExpressions;
 
 namespace PatrickBotman.Models;
 
-public class TextInput {
-    public string FirstLine {get; set; } = string.Empty;
+public class TextInput
+{
+    public string FirstLine { get; set; } = string.Empty;
     public string SecondLine { get; set; } = string.Empty;
     private readonly int _maximumTextLength;
 
     public TextInput(string text, IConfiguration configuration)
     {
         _maximumTextLength = configuration.GetValue<int>("MaximumTextLength");
-        PrepareText(text); 
+        PrepareText(text);
     }
 
     private void PrepareText(string text)
     {
         text = text.Trim().ToUpper().Substring(0, Math.Min(_maximumTextLength, text.Length));
         var newLineChar = text.IndexOf('\n');
-        if(newLineChar != -1 && text.IndexOf('\n', newLineChar + 1) != -1)
-            {
-                text = text.Substring(0, text.IndexOf('\n', newLineChar));
-            }
+        if (newLineChar != -1 && text.IndexOf('\n', newLineChar + 1) != -1)
+        {
+            text = text.Substring(0, text.IndexOf('\n', newLineChar));
+        }
 
         int separationIndex = 0;
-        for(int i = 0; i < text.Length; i++) {   
-            if(Char.IsSeparator(text[i])) {
-                if(Math.Abs(text.Length/2 - i) <  Math.Abs(text.Length/2 - separationIndex)) 
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (Char.IsSeparator(text[i]))
+            {
+                if (Math.Abs(text.Length / 2 - i) < Math.Abs(text.Length / 2 - separationIndex))
                     separationIndex = i;
             }
         }
         if (text[separationIndex] != '\n')
             text = text.Replace("\n", string.Empty);
+        if (separationIndex > 0)
+        {
+            FirstLine = text.Substring(0, separationIndex + 1);
+            SecondLine = text.Substring(separationIndex + 1, text.Length - FirstLine.Length);
+        }
+        else
+        {
+            FirstLine = text;
+            SecondLine = string.Empty;
+        }
 
-        FirstLine = text.Substring(0, separationIndex + 1);
-        SecondLine = text.Substring(separationIndex + 1, text.Length - FirstLine.Length);    
+        var regex = new Regex("[}{:;\"'`]");
 
-        var regex = new Regex("[}{:;\"'`]");  
-        
         FirstLine = regex.Replace(FirstLine, string.Empty);
         SecondLine = regex.Replace(SecondLine, string.Empty);
     }
