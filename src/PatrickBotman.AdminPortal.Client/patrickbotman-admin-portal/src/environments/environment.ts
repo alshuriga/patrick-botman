0009151d9cb14d69ac0234483789b825f944ada2
Oleksandr Shuryha <alshuriga@gmail.com>
2024-11-23 21:49:21 +0100

    update client url

--- src/PatrickBotman.AdminPortal.Client/patrickbotman-admin-portal/src/environments/environment.ts
+++ src/PatrickBotman.AdminPortal.Client/patrickbotman-admin-portal/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     API_BASE_URL: "/api/",
-    CLIENT_BASE_URL: "https://patrick-botman.alshuriga.ink/admin/",
+    CLIENT_BASE_URL: "https://patrick-botman.alshuriga.fyi/admin/",
 };
